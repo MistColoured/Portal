7ed46a982dd3545dc54afb27c5840dfb65502eae
Michael Allen <mike@mistColoured.com>
2018-07-03 18:32:20 +0100

    Initial commit

--- App.jsx
+++ App.jsx
@@ -1,23 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 
-import Toggle from './RenderProps/ToggleChildren';
-import Portal from './RenderProps/Portal';
+import Modal from './Portal/Portal';
 
-const App = () => (
-  <div>
-    <Toggle>
-      {({ on, toggle }) => (
-        <Fragment>
-          {on && <h1>Shown Me</h1>}
-          <button onClick={toggle}>Show/Hidden</button>
-          <Portal>
-            {on && <h1>Hi, I am a Portal</h1>}
-          </Portal>
-        </Fragment>
-      )}
-    </Toggle>
+class App extends Component {
+  state = {
+    showModal: false,
+  };
 
-  </div>
-);
+  handleShow = () => {
+    this.setState({ showModal: true });
+  }
+
+  handleHide = () => {
+    this.setState({ showModal: false });
+  }
+
+  render() {
+    const { showModal } = this.state;
+    // Show a Modal on click.
+    // (In a real app, don't forget to use ARIA attributes
+    // for accessibility!)
+    const modal = showModal ? (
+      <Modal>
+        <div>
+          <div>
+            With a portal, we can render content into a different
+            part of the DOM, as if it were any other React child.
+          </div>
+          This is being rendered inside the #modal-container div.
+          <button onClick={this.handleHide}>Hide modal</button>
+        </div>
+      </Modal>
+    ) : null;
+
+    return (
+      <div className="app">
+        This div has overflow: hidden.
+        <button onClick={this.handleShow}>Show modal</button>
+        {modal}
+      </div>
+    );
+  }
+}
 
 export default App;
